5a02a8b4be82f2650ae8e7d5d94b1e7ac46f040c
CodeMogule <codemogule@gmail.com>
2021-07-30 19:52:19 +0300

    shorten Link Web App

--- JS/app.js
+++ JS/app.js
@@ -6,7 +6,14 @@ const elements = {
     body: document.querySelector('body'),
     textBar: document.querySelector('.link-text-bar'),
     form: document.querySelector('form'),
-    linkUi: document.getElementById('information')
+    linkUi: document.getElementById('information'),
+    errorMessage: document.querySelector('.error-message'),
+    shortenItBtn: document.querySelector('.shorten-it-btn'),
+    shortenItText: document.querySelector('.shorten-it'),
+    spinner: document.querySelector('.spinner'),
+    loader: 'loader',
+    copyBtn : `copy-btn`,
+    copyText: `copy`,
 }
 
 //open mobile menu
@@ -29,28 +36,87 @@ elements.body.classList.remove('minimize');
 
 
 //shortner link  
-
 async function getLink(url){
     const data = await axios(`https://api.shrtco.de/v2/shorten?url=${url}`)
-    
-    const markup = `
+    const markup =
+    `
     <div class="link-result" >
     <h4>${data.data.result.original_link}</h4>
     <div class = "link-result-line"style="border-top: 1px solid rgba(122, 122, 122, 0.363);" ></div>
-    <a href="${url}"><h5>${data.data.result.full_short_link}</h5></a>
-    <button class="copy-btn">
-        Copy
-    </button>
+    <a href="${url}"><h5 class = ${elements.linkResultText}>${data.data.result.full_short_link}</h5></a>
+    <button class="${elements.copyBtn}" onclick = "copyText()">
+       <span id = "${elements.copyText}">Copy</span>
+    </button
 </div>
-  
 `
 elements.linkUi.insertAdjacentHTML("afterbegin",markup)
+
+window.copyText = function(){
+    var input  =document.createElement('input');
+document.body.appendChild(input)
+input.value = data.data.result.full_short_link;
+input.select();
+document.execCommand('copy',false);
+input.remove();
+let copyBtn = document.querySelector(`.${elements.copyBtn}`)
+let copyText = document.getElementById(`${elements.copyText}`)
+copyBtn.style.backgroundColor = 'hsl(257, 27%, 26%)';
+copyText.textContent = 'Copied!'
+}
 }
 
-        elements.form.addEventListener('submit', (event)=> {
+function loader(){
+    elements.shortenItText.style.display = 'none';
+    let markup = `
+    <div class = ${elements.loader}>
+    <i class="fas fa-spinner spinner"></i>
+    </div>
+    `
+    elements.shortenItBtn.innerHTML = markup
+}
+
+function clearLoader(){
+    const loader =  document.querySelector(`.${elements.loader}`)
+    if(loader) loader.parentElement.removeChild(loader)
+    elements.shortenItBtn.innerHTML = `<span>Shorten it!</span>`
+}
+
+function urlValidity(string){
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(string);
+}
+
+
+
+
+async function shorterLink(){
+    let text = elements.textBar.value;
+    if(text === ''){
+        elements.errorMessage.style.display = 'block';
+        elements.textBar.style.borderColor = 'red';
+    }else if(urlValidity(text) === false){
+        elements.errorMessage.style.display = 'block';
+        elements.textBar.style.borderColor = 'red';
+    }
+    
+    else if(urlValidity(text) === true) {
+        elements.errorMessage.style.display = 'none';
+        elements.textBar.style.borderColor = 'initial';
+        loader()
+        elements.textBar.value = ''
+    await getLink(text)
+    clearLoader()
+    }
+}
+
+        elements.form.addEventListener('submit',(event)=> {
             event.preventDefault()
-            let text = elements.textBar.value;
-            getLink(text)
+           shorterLink()
         })
 
 
